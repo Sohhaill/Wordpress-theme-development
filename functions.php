aadf211752c01526ff3ba12a5ca90303440ad09e
Sohail <141762201+Sohhaill@users.noreply.github.com>
2025-03-18 15:18:59 +0500

    Remove Extra Code

--- functions.php
+++ functions.php
@@ -23,10 +23,6 @@ function wplearning_theme_script()
 add_action('wp_enqueue_scripts', 'wplearning_theme_script');
 
 
-if (!defined('_S_VERSION')) {
-	// Replace the version number of the theme on each release.
-	define('_S_VERSION', '1.0.0');
-}
 
 /**
  * Sets up theme defaults and registers support for various WordPress features.
@@ -95,68 +91,12 @@ function resilience_setup()
 }
 add_action('after_setup_theme', 'resilience_setup');
 
-/**
- * Set the content width in pixels, based on the theme's design and stylesheet.
- *
- * Priority 0 to make it available to lower priority callbacks.
- *
- * @global int $content_width
- */
-function resilience_content_width()
-{
-	$GLOBALS['content_width'] = apply_filters('resilience_content_width', 640);
-}
-add_action('after_setup_theme', 'resilience_content_width', 0);
 
-/**
- * Register widget area.
- *
- * @link https://developer.wordpress.org/themes/functionality/sidebars/#registering-a-sidebar
- */
-
-/**
- * Enqueue scripts and styles.
- */
-function resilience_scripts()
-{
-	wp_enqueue_style('resilience-style', get_stylesheet_uri(), array(), _S_VERSION);
-	wp_style_add_data('resilience-style', 'rtl', 'replace');
-
-	wp_enqueue_script('resilience-navigation', get_template_directory_uri() . '/assets/js/navigation.js', array(), _S_VERSION, true);
-
-	if (is_singular() && comments_open() && get_option('thread_comments')) {
-		wp_enqueue_script('comment-reply');
-	}
-}
-add_action('wp_enqueue_scripts', 'resilience_scripts');
-
-/**
- * Implement the Custom Header feature.
- */
-require get_template_directory() . '/inc/custom-header.php';
-
-/**
- * Custom template tags for this theme.
- */
-require get_template_directory() . '/inc/template-tags.php';
-
-/**
- * Functions which enhance the theme by hooking into WordPress.
- */
-require get_template_directory() . '/inc/template-functions.php';
-
-/**
- * Customizer additions.
- */
-require get_template_directory() . '/inc/customizer.php';
 require get_template_directory() . '/inc/Courses.php';
 require get_template_directory() . '/inc/Events.php';
 /**
  * Load Jetpack compatibility file.
  */
-if (defined('JETPACK__VERSION')) {
-	require get_template_directory() . '/inc/jetpack.php';
-}
 
 function home_sidebar()
 {
